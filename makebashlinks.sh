f49688ab447125fc473e57b3a3d7e1daa3f32cc9
Lukasz Lewandowski <ivanhoe1982@gmail.com>
2015-01-13 01:56:08 +0100

    initial configs

---
+++ makebashlinks.sh
@@ -0,0 +1,7 @@
+rm ~/.bash_profile
+rm ~/.bashrc
+rm ~/.gitignore_global
+
+ln -s ~/Dropbox/dev/common/.bash_profile ~/.bash_profile
+ln -s ~/Dropbox/dev/common/.bashrc ~/.bashrc
+ln -s ~/Dropbox/dev/common/.gitignore_global ~/.gitignore_global
